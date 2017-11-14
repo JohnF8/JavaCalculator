600b78cd4a4872d9d031c1e6215fdd79224807e4
JOhn <johnnf8@gmail.com>
2017-11-13 20:54:52 -0500

    Added new UI elements

--- SimpleCalc.java
+++ SimpleCalc.java
@@ -20,23 +20,40 @@ public class SimpleCalc
 {
 	JFrame window;  
 	Container content ;
-	JButton[] digits = new JButton[13]; 
-	JButton[] ops = new JButton[5];
 	JTextField expression;
 	JTextField result;
-	String[] opCodes = { "+", "-", "*", "/", "%" };
-	int NUM_OPS=5;
+
+	JButton add;
+	JButton subtract;
+	JButton multiply;
+	JButton divide;
+	JButton lParen;
+	JButton rParen;
+	JButton exponent;
+	JButton exp;
+	JButton ln;
+	JButton log;
+	JButton sqrt;
+	JButton cos;
+	JButton sin;
+	JButton tan;
+	JButton clear;
+	JButton enter;
+	
 	public SimpleCalc()
 	{
+		//Setting up the frame
 		window = new JFrame( "Simple Calc");
 		content = window.getContentPane();
+
+		//Two windows are within content
 		content.setLayout(new GridLayout(2,1)); 
 		ButtonListener listener = new ButtonListener();
 		
-		
-		JPanel topPanel = new JPanel();
-		topPanel.setLayout(new GridLayout(2,1)); 
-		
+		//Top panel		
+		JPanel topPanel = new JPanel(); //Top panel that will hold the input and output
+		topPanel.setLayout(new GridLayout(2,1));
+
 		expression = new JTextField();
 		expression.setFont(new Font("verdana", Font.BOLD, 16));
 		expression.setText("");
@@ -47,49 +64,99 @@ public class SimpleCalc
 		
 		topPanel.add(expression);
 		topPanel.add(result);
-						
+	
+		//Bottom Panel
 		JPanel bottomPanel = new JPanel();
-		bottomPanel.setLayout(new GridLayout(1,2));  
-	
-		JPanel  digitsPanel = new JPanel();
-		digitsPanel.setLayout(new GridLayout(5,3));	
+		bottomPanel.setLayout(new GridLayout(4,4));
+
+		//Numerous Components of Bottom Panel
+		add = new JButton();
+		add.setText("+");
+		add.addActionListener(listener);
+
+		subtract = new JButton();
+		subtract.setText("-");
+		subtract.addActionListener(listener);
+
+		multiply = new JButton();
+		multiply.setText("*");
+		multiply.addActionListener(listener);
+
+		divide = new JButton();
+		divide.setText("/");
+		divide.addActionListener(listener);
 		
-		for (int i=1 ; i<10 ; i++ )
-		{
-			digits[i-1] = new JButton( ""+i );
-			digitsPanel.add( digits[i-1] );
-			digits[i-1].addActionListener( listener );
-		}
-		digits[9] = new JButton( "C" );
-		digitsPanel.add( digits[9] );
-		digits[9].addActionListener( listener ); 
-
-		digits[10]= new JButton("0");
-		digitsPanel.add(digits[10]);
-		digits[10].addActionListener(listener);
-
-		digits[11] = new JButton( "CE" );
-		digitsPanel.add( digits[11] );
-		digits[11].addActionListener( listener ); 		
-
-		digits[12] = new JButton("Enter");
-		digitsPanel.add(digits[12]);
-		digits[12].addActionListener(listener);
-
-		JPanel opsPanel = new JPanel();
-		opsPanel.setLayout(new GridLayout(5,1));
-		for (int i=0 ; i<5 ; i++ )
-		{
-			ops[i] = new JButton( opCodes[i] );
-			opsPanel.add( ops[i] );
-			ops[i].addActionListener( listener ); 
-		}
-		bottomPanel.add( digitsPanel );
-		bottomPanel.add( opsPanel );
+		lParen = new JButton();
+		lParen.setText("(");
+		lParen.addActionListener(listener);
 		
-		content.add( topPanel );
-		content.add( bottomPanel );
-	
+		rParen = new JButton();
+		rParen.setText(")");
+		rParen.addActionListener(listener);
+		
+		exponent = new JButton();
+		exponent.setText("^");
+		exponent.addActionListener(listener);
+
+		exp = new JButton();
+		exp.setText("e");
+		exp.addActionListener(listener);
+		
+		ln = new JButton();
+		ln.setText("ln");
+		ln.addActionListener(listener);
+
+		log = new JButton();
+		log.setText("log");
+		log.addActionListener(listener);
+
+		sqrt = new JButton();
+		sqrt.setText("sqrt");
+		sqrt.addActionListener(listener);
+
+		cos = new JButton();
+		cos.setText("cos");
+		cos.addActionListener(listener);
+		
+		sin = new JButton();
+		sin.setText("sin)");
+		sin.addActionListener(listener);
+
+		tan = new JButton();
+		tan.setText("tan");
+		tan.addActionListener(listener);
+		
+		clear = new JButton();
+		clear.setText("CE");
+		clear.addActionListener(listener);
+
+		enter = new JButton();
+		enter.setText("enter");
+		enter.addActionListener(listener);
+		
+		bottomPanel.add(add);
+		bottomPanel.add(subtract);
+		bottomPanel.add(multiply);
+		bottomPanel.add(divide);
+		
+		bottomPanel.add(lParen);
+		bottomPanel.add(rParen);
+		bottomPanel.add(exponent);
+		bottomPanel.add(exp);
+
+
+		bottomPanel.add(ln);
+		bottomPanel.add(log);
+		bottomPanel.add(sqrt);
+		bottomPanel.add(cos);
+		
+		bottomPanel.add(sin);
+		bottomPanel.add(tan);
+		bottomPanel.add(clear);
+		bottomPanel.add(enter);
+		
+		content.add(topPanel);
+		content.add(bottomPanel);
 		window.setSize( 640,480);
 		window.setVisible( true );
 	}
@@ -99,29 +166,12 @@ public class SimpleCalc
 		public void actionPerformed(ActionEvent e)
 		{
 			Component whichButton = (Component) e.getSource();
-			for (int i=1 ; i<10 ; i++ )
-				if (whichButton == digits[i-1])
-					expression.setText( expression.getText() + i );
 
-			if(whichButton == digits[9]){
-				expression.setText("");
+			if(whichButton == cos){
+				expression.setText(expression.getText() + "cos( ");
 			}
 
-			if(whichButton == digits[10]){
-				expression.setText(expression.getText() +0);
-			}
-			if(whichButton == digits[11]){
-				String curExp = expression.getText();
-				curExp = curExp.substring(0, curExp.length()-1);
-			expression.setText(curExp);
-			}
-
-			for(int j=0; j<NUM_OPS; j++){
-				if(whichButton == ops[j])
-					expression.setText(expression.getText()+opCodes[j]);
-			}
-
-			if(whichButton == digits[12]){
+			if(whichButton == enter){
 				evaluate();	
 			}
 			
@@ -143,51 +193,6 @@ public class SimpleCalc
 					operands.add(Double.parseDouble(token));
 		}
 
-		for(int i=0; i<operators.size(); i++){
-			if(operators.get(i).equals("^")){
-				operands.set(i, Math.pow(operands.get(i), operands.get(i+1)));
-				operands.remove(i+1);
-				operators.remove(i);
-				i--;
-			}
-		}
-		for(int i=0; i<operators.size(); i++){
-			if(operators.get(i).equals("*")){
-				operands.set(i, operands.get(i) * operands.get(i+1));
-				operands.remove(i+1);
-				operators.remove(i);
-				i--;
-			}
-			
-			else if(operators.get(i).equals("/")){
-				operands.set(i, operands.get(i) / operands.get(i+1));
-				operands.remove(i+1);
-				operators.remove(i);
-				i--;
-			}
-
-			else if(operators.get(i).equals("%")){
-					operands.set(i, operands.get(i) % operands.get(i+1));
-					operands.remove(i+1);
-					operators.remove(i);
-					i--;
-			}
-		}
-
-		for(int x=0; x<operators.size(); x++){
-			if(operators.get(x).equals("+")){
-				operands.set(x, operands.get(x) + operands.get(x+1));
-				operands.remove(x+1);
-				operators.remove(x);
-				x--;
-			}
-			else if(operators.get(x).equals("-")){
-				operands.set(x, operands.get(x) - operands.get(x+1));
-				operands.remove(x+1);
-				operators.remove(x);
-				x--;
-			}
-		}
 		String finalRes = operands.get(0).toString();
 		if(operators.size() == 0 || operands.size() == 1)
 				result.setText(finalRes);
